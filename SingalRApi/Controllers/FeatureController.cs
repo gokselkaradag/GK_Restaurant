aba51a7df538a8dfc5b50bab3c23aeba671acb40
Göksel Karadağ <goksel.karadag34@gmail.com>
2024-12-14 17:59:37 +0300

    ApiControllerFinished

---
+++ SingalRApi/Controllers/FeatureController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SingalR.BusinessLayer.Abstract;
+using SingalR.DtoLayer.FeatureDto;
+using SingalR.EntityLayer.Entities;
+
+namespace SingalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IFeatureService _featureService;
+        private readonly IMapper _mapper;
+
+        public FeatureController(IFeatureService featureService, IMapper mapper)
+        {
+            _featureService = featureService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _mapper.Map<List<ResultFeatureDto>>(_featureService.TGetListAll());
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            _featureService.TAdd(new Feature()
+            {
+                DescriptionOne = createFeatureDto.DescriptionOne,
+                DescriptionTwo = createFeatureDto.DescriptionTwo,
+                DescriptionThree = createFeatureDto.DescriptionThree,
+                TitleOne = createFeatureDto.TitleOne,
+                TitleTwo = createFeatureDto.TitleTwo,
+                TitleThree = createFeatureDto.TitleThree,
+            });
+
+            return Ok("Özellikler Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _featureService.TGetByID(id);
+            _featureService.TDelete(value);
+            return Ok("Özellikler Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            _featureService.TUpdate(new Feature()
+            {
+                FeatureID = updateFeatureDto.FeatureID,
+                DescriptionOne = updateFeatureDto.DescriptionOne,
+                DescriptionTwo = updateFeatureDto.DescriptionTwo,
+                DescriptionThree = updateFeatureDto.DescriptionThree,
+                TitleOne = updateFeatureDto.TitleOne,
+                TitleTwo = updateFeatureDto.TitleTwo,
+                TitleThree = updateFeatureDto.TitleThree,
+            });
+
+            return Ok("Özellikler Güncellendi");
+        }
+
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var values = _featureService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
--- SingalRApi/Program.cs
+++ SingalRApi/Program.cs
@@ -31,6 +31,18 @@ namespace SingalRApi
             builder.Services.AddScoped<IDiscountService, DiscountManager>();
             builder.Services.AddScoped<IDiscountDal, EfDiscountDal>();
 
+            builder.Services.AddScoped<IFeatureService, FeatureManager>();
+            builder.Services.AddScoped<IFeatureDal, EfFeatureDal>();
+
+            builder.Services.AddScoped<IProductService, ProductManager>();
+            builder.Services.AddScoped<IProductDal, EfProductDal>();
+
+            builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
+            builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
+
+            builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+            builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
