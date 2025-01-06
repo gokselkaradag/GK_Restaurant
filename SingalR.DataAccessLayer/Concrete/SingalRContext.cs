22a6e7759d28f3020e4e6e33214b53c3f9201f71
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-06 16:26:27 +0300

    Basket

--- SingalR.DataAccessLayer/Concrete/SingalRContext.cs
+++ SingalR.DataAccessLayer/Concrete/SingalRContext.cs
@@ -24,6 +24,7 @@ namespace SingalR.DataAccessLayer.Concrete
         public DbSet<MoneyCase> MoneyCases { get; set; }
         public DbSet<MenuTable> MenuTables { get; set; }
         public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
 
     }
 }
