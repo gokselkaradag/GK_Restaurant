3c12a77115f91bb3ed6eee0e0f3142be4bc3da23
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-13 15:38:16 +0300

    ReservationTransactions

--- SingalRApi/Hubs/SignalRHub.cs
+++ SingalRApi/Hubs/SignalRHub.cs
@@ -11,14 +11,16 @@ namespace SingalRApi.Hubs
         private readonly IOrderService _orderService;
         private readonly IMoneyCaseService _moneyCaseService;
         private readonly IMenuTableService _menuTableService;
+        private readonly IBookingService _bookingService;
 
-        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService)
         {
             _categoryService = categoryService;
             _productService = productService;
             _orderService = orderService;
             _moneyCaseService = moneyCaseService;
             _menuTableService = menuTableService;
+            _bookingService = bookingService;
         }
 
         public async Task SendStatistic()
@@ -80,5 +82,11 @@ namespace SingalRApi.Hubs
             var valuemenu = _menuTableService.TMenuTableCount();
             await Clients.All.SendAsync("ReceiveMenuTableCount", valuemenu);
         }
+
+        public async Task GetBookingList()
+        {
+            var values = _bookingService.TGetListAll();
+            await Clients.All.SendAsync("ReceiveBookingList", values);
+        } 
     }
 }
